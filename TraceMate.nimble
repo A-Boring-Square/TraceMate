a84fa4837b3250c75e856a648b8e0efe4eef9199
A-Boring-Square <123091707+A-Boring-Square@users.noreply.github.com>
2024-10-19 15:41:51 -0500

    init commit

---
+++ TraceMate.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "A-Boring-Square"
+description   = "TraceMate is a tiny application meant to run alongside other programs to be used as a logging system"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["TraceMate"]
+
+
+# Dependencies
+
+requires "nim >= 2.0.8"
---
+++ src/TraceMate.nim
@@ -0,0 +1,41 @@
+import base64, json, times, os
+
+type
+  LoggerConfig = object
+    LoggerName: string
+    DisplayTime: bool
+    Level: string
+    LoggerText: string
+
+proc processLog(config: LoggerConfig) =
+  let timeDisplay = if config.DisplayTime: 
+                      "Current Time: " & $getTime() 
+                    else: 
+                      ""
+  echo(timeDisplay & "[" & config.Level & "] " & config.LoggerName & ": " & config.LoggerText)
+
+when isMainModule:
+  # Check if a command-line argument is provided
+  if paramCount() < 1:
+    echo("Usage: nim c -r your_file.nim <base64_encoded_json>")
+    quit(1)
+
+  # Get the Base64 encoded JSON string from command-line argument
+  let base64String = paramStr(1)
+
+  # Decode Base64 to JSON string
+  var decodedString = base64.decode(base64String)
+  
+  # Parse JSON
+  let jsonData = parseJson(decodedString)
+  
+  # Create LoggerConfig object from JSON
+  let config = LoggerConfig(
+    LoggerName: jsonData["LoggerName"].getStr(),
+    DisplayTime: jsonData["DisplayTime"].getBool(),
+    Level: jsonData["Level"].getStr(),
+    LoggerText: jsonData["LoggerText"].getStr()
+  )
+
+  # Process the log based on the config
+  processLog(config)
